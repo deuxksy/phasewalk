ddf3f7055560f350a907ca91b96b169869efd654
SeokYoung Kim <crom@zzizily.com>
2017-03-03 00:50:01 +0900

    shell 수정

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,6 +4,8 @@ export PATH="${HOME}/.pyenv/bin:$PATH"
 export ZZIZILY_PHASEWALK_CRYPTO=FJnIOiEXv4owpcej6zFrBEGhwRG8oNVI-3yonQu9vN8=
 export ZZIZILY_PHASEWALK_HOME=${HOME}/apps/phasewalk
 
+cd ${ZZIZILY_PHASEWALK_HOME}
+
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 source `which activate.sh`
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-source ./init.sh && ./run.sh && ./destroy.sh
+export ZZIZILY_PHASEWALK_CRYPTO=FJnIOiEXv4owpcej6zFrBEGhwRG8oNVI-3yonQu9vN8=
+export ZZIZILY_PHASEWALK_HOME=${HOME}/apps/phasewalk
+export ZZIZILY_PHASEWALK_MODE=prod
+
+cd ${ZZIZILY_PHASEWALK_HOME}
+
+source ${ZZIZILY_PHASEWALK_HOME}/scripts/init.sh && ${ZZIZILY_PHASEWALK_HOME}/scripts/run.sh && ${ZZIZILY_PHASEWALK_HOME}/scripts/destroy.sh
