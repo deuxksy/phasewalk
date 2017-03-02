284844dc80679fce653d3368446fee7659bb305f
SeokYoung Kim <crom@zzizily.com>
2017-03-02 17:42:10 +0900

    script 수정

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+
 export PATH="${HOME}/.pyenv/bin:$PATH"
+export ZZIZILY_PHASEWALK_CRYPTO=FJnIOiEXv4owpcej6zFrBEGhwRG8oNVI-3yonQu9vN8=
+export ZZIZILY_PHASEWALK_HOME=${HOME}/apps/phasewalk
+
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 source `which activate.sh`
