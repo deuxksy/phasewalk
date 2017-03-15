2f98a37be3adcd517cdc70276601535e6f62b556
SeokYoung Kim <crom@zzizily.com>
2017-03-16 05:08:24 +0900

    미사용 resources 사제 및 config local 환경에 맞추어서 갱신

--- phasewalk/config.py
+++ phasewalk/config.py
@@ -1,22 +1,24 @@
+# !/usr/bin/env python
+#-*- coding: utf-8 -*-
 config = {
     "db": {
         "redis_db_common": 0,
-        "redis_host": "gAAAAABYuC1Jk_EbBfUgAsmTLk5Q5RAPqdmu8r1QI0DXmY7JSxIJEFo-bVf3Ll1dwK_wi6TBn0RkFB97TefOgX9mujRAyORbs_eHJTJZ45v9FPEwYVUFeOE=",
-        "redis_password": "gAAAAABYuC1JT0NU4FmRhSJ98EVF8zxmN8iq6G67VZcPwe1Hbag8LDbE_y6DhO09VeHCsUaEZkSzppwGiA9xwcPoFTFnLTcNzA==",
-        "redis_port": "gAAAAABYuC1JataO9LiDiLzx3cAwpXbjqtC0CuEwfx9z7B4is9ls4LSRS49lqM66QGqbCumLNJg26blvEptEOu-XCRzUDn1Siw=="
+        "redis_host": "gAAAAABYyZ7uhvOZzHF6zlsxVPoFnMI_gnGKNrwXmpQD6RBnMioGtEp8W4Ekqduss09qjYZqVNcNDFKGgjhGW9Q4itws9xF7ULqtLGvehi9LZf06QVwT6BE=",
+        "redis_password": "gAAAAABYyZ7ubhGfzKLfJ77HVYla6nyBPbjkMKBZJ9sH9VY4PHzbQHjYRNm3fnaj2QE10hRGfaVyYFiqRSYaJsKG9DOnUeds-g==",
+        "redis_port": "gAAAAABYyZ7u5MwCxudqn8WeG1qeiAPCgboVC5tkdcG93XjFaEyOiPeH3jvmkGZ7P95SKSBZI31rBfqm5mJ59-9XT0UoS_peQg=="
     },
     "default": {
         "browser": "phantomjs",
-        "chrome_executable_path": "",
-        "firefox_executable_path": "",
-        "mode": "prod",
-        "phantomjs_executable_path": "/home/pi/apps/phasewalk/resources/phantomjs/2.1.1/phantomjs"
+        "chrome_executable_path": "D:/DK/Version/Git/ZZiZiLY/Python/phasewalk/phasewalk/resources/chromedriver/2.27/chromedriver.exe",
+        "firefox_executable_path": "D:/DK/Version/Git/ZZiZiLY/Python/phasewalk/phasewalk/resources/geckodriver/geckodriver.exe",
+        "mode": "local",
+        "phantomjs_executable_path": "D:/DK/Version/Git/ZZiZiLY/Python/phasewalk/phasewalk/resources/phantomjs/2.1.1/phantomjs.exe"
     },
     "log": {
         "encoding": "utf-8",
         "file_size": 128,
         "formatter": "[%(levelname)s|%(filename)s:%(lineno)s] %(asctime)s > %(message)s",
-        "path": "/home/pi/apps/phasewalk/logs",
+        "path": "D:/DK/Version/Git/ZZiZiLY/Python/phasewalk/logs",
         "prefix": "%Y/%m/%d",
         "suffix": "%H%M%S"
     }
